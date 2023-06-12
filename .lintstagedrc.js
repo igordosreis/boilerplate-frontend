f74ffa35d3b3e2933d692ebb45ce90592b1151ab
Igor dos Reis <igorreis@gmail.com>
2023-06-12 13:57:23 -0300

    test: test

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  '*/.{js,ts}?(x)': [
+    filenames =>
+      `next lint --fix --file ${filenames
+        .map(file => file.split(process.cwd())[1])
+        .join(' --file ')}`,
+    filenames => `prettier --write ${filenames.join(' ')}`,
+    () => 'tsc --noEmit --skipLibCheck',
+  ],
+  '*.scss': ['stylelint --syntax scss --fix'],
+}
